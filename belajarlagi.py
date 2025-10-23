289c78dc4f9d9770e8109f8b348ac3efd11c88c9
Ahmad Rifai <ahmadrifaipaiy123@gmail.com>
2025-10-23 10:40:33 +0700

    hapus

---
+++ belajarlagi.py
@@ -0,0 +1,39 @@
+
+### looping
+# # while True:
+# #     try:
+# #         nilai = int(input("masukkan nilai :"))
+# #         if nilai<10:
+# #             print("tidak lulus!")
+# #         else:
+# #             print("lulus")
+# #             break
+# #     except ValueError:
+# #        print("ulangi")
+
+# while True: 
+#     try:
+#        y = int(input("masukkan nilai:"))
+#        p = int(input("masukkan nilai:"))
+#        x = p+y
+#        print("hasil nya adalah :",x, "unit")
+#        if x<30:
+#             print("ulangi")
+#        else:
+#             print("berhasil")  
+#             break
+#     except ValueError:
+#         print("ulangi")
+
+##operator perbandingan
+# a = 5
+# b = 5
+
+# if a!=b:
+#     print("selesai")
+# else:
+#     print("salah")
+
+# x = 5
+# x += 6 #sama dengan: x = x + 6
+
